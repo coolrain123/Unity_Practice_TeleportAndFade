4083e4059f287dad3af7cef3147378b473bb2943
coolrain123 <59252383+coolrain123@users.noreply.github.com>
2020-01-16 16:25:44 +0800

    轉場

---
+++ Unity_TeleportAndFade/Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Scene Scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene = FindObjectOfType<Scene>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("載入下ㄧ關");
+        if (other.name == "傳送裝置")
+        {
+            StartCoroutine(Scene.NextLevel());
+        }
+    }
+}
---
+++ Unity_TeleportAndFade/Assets/Scene.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Scene : MonoBehaviour
+{
+
+    private Image imgcross;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imgcross = GameObject.Find("轉場效果").GetComponent<Image>();
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+    public IEnumerator NextLevel()
+    {
+        for (int k = 0; k < 50; k++)
+        {
+            imgcross.color += new Color(0, 0, 0, 0.03f);
+            yield return new WaitForSeconds(0.02f);
+        }
+        
+
+        SceneManager.LoadScene("第二關");
+        yield return new WaitForSeconds(1f);
+        for (int k = 0; k < 50; k++)
+        {
+            imgcross.color -= new Color(49, 49, 49, 0.03f);
+            yield return new WaitForSeconds(0.02f);
+        }
+        
+    }
+}
